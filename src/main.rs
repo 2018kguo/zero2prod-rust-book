902206f47ecc4127e45edb8e9137b3537fc71ee1
Kevin Guo <kguo@ramp.com>
2023-11-26 14:19:29 -0500

    integration test working

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+//! src/main.rs
+//!
+use std::net::TcpListener;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    run(listener)?.await
 }
